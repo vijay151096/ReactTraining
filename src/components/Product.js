5e984a62c5c295111c5c9e2e46fe77c379824d30
P M Mohamed Jaseem Jabir <pmmohamed.j@thoughtworks.com>
2022-12-26 10:24:16 +0530

    functionality to change the quantity

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,57 +1,82 @@
 import React from "react";
 import { Card, Button } from "semantic-ui-react";
 import { useState } from "react";
-import {CardActions, CardContent, CardMedia} from "@mui/material";
+import { CardActions, CardContent, CardMedia } from "@mui/material";
 import Typography from "@mui/material/Typography";
+import classes from "./Product.module.css";
 
 function Product({ item }) {
-  const [isAdded, setIsAdded] = useState(false);
+  const [quantity, setQuantity] = useState(1);
 
   const handleClick = async (e, id) => {
-    setIsAdded((prevState) => !prevState);
-    if (!isAdded) {
-        fetch("http://localhost:8080/cart", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(item),
-        })
-            .then((response) => response.json())
-            .then(() => {
-            })
-            .catch((error) => {
-                console.error("Error:", error);
-            });
-    } else {
-        fetch(`http://localhost:8080/cart/${id}`, {
-            method: "DELETE"
-        })
-            .then(() => {})
-            .catch((error) => {
-                console.error("Error:", error);
-            });
-    }
+    const bodyToSent = { ...item, quantity: quantity };
+
+    fetch("http://localhost:8080/cart", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(bodyToSent),
+    })
+      .then((response) => response.json())
+      .then(() => {})
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   };
 
   return (
-      <Card sx={{ maxWidth: 345 }}>
-          <CardMedia
-              component="img"
-              alt="green iguana"
-              height="140"
-              image={`https://picsum.photos/200/300?random=${item.id}`}
-          />
-          <CardContent>
-              <Typography gutterBottom variant="h5" component="div"> {item.name} </Typography>
-              <Typography variant="body3" color="text.secondary"> {item.meta} </Typography>
-              <Typography variant="body2" color="text.secondary"> Rs. {item.price} </Typography>
-          </CardContent>
-          <CardActions>
-                   {  ( !isAdded && <Button size="small" onClick={(e) => handleClick(e, item.id)} primary>Add</Button> )
-                  ||  ( isAdded && <Button size="small" onClick={(e) => handleClick(e, item.id)}>Remove</Button> ) }
-          </CardActions>
-      </Card>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="green iguana"
+        height="140"
+        image={`https://picsum.photos/200/300?random=${item.id}`}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {" "}
+          {item.name}{" "}
+        </Typography>
+        <Typography variant="body3" color="text.secondary">
+          {" "}
+          {item.meta}{" "}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {" "}
+          Rs. {item.price}{" "}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <div className={classes.alignButtons}>
+          <div>
+            <i
+              className={`minus icon ${classes.iconCursor}`}
+              onClick={() => {
+                setQuantity((prevQuantity) => {
+                  if (prevQuantity > 1) {
+                    return prevQuantity - 1;
+                  }
+                  return prevQuantity;
+                });
+              }}
+            ></i>
+            <p className={classes.chgInline}>{quantity}</p>
+            <i
+              className={`plus icon ${classes.iconCursor}`}
+              onClick={() => {
+                setQuantity((prevQuantity) => {
+                  return prevQuantity + 1;
+                });
+              }}
+            ></i>
+          </div>
+          <Button size="small" onClick={(e) => handleClick(e, item.id)} primary>
+            Add
+          </Button>
+        </div>
+      </CardActions>
+    </Card>
   );
 }
 
