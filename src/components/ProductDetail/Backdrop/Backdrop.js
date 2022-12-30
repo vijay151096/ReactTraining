35f86cf0ce05138a4cf49b24b1bc5c10806452dd
P M Mohamed Jaseem Jabir <pmmohamed.j@thoughtworks.com>
2022-12-30 11:47:29 +0530

    created styled component using emotion

--- src/components/ProductDetail/Backdrop/Backdrop.js
+++ src/components/ProductDetail/Backdrop/Backdrop.js
@@ -1,7 +1,17 @@
-import classes from "./Backdrop.module.css";
+import styled from "styled-components";
 
 const Backdrop = (props) => {
-  return <div onClick={props.handleClick} className={classes.backdrop} />;
+  const StyledBackdrop = styled.div({
+    position: "fixed",
+    top: 0,
+    left: 0,
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: "rgba(128, 128, 128, 0.53)",
+    zIndex: 10,
+  });
+
+  return <StyledBackdrop onClick={props.handleClick} />;
 };
 
 export default Backdrop;
--- src/components/ProductDetail/ProductDetail/ProductDetail.js
+++ src/components/ProductDetail/ProductDetail/ProductDetail.js
@@ -1,4 +1,3 @@
-import classes from "./ProductDetail.module.css";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -6,36 +5,48 @@ import CardMedia from "@mui/material/CardMedia";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Divider } from "semantic-ui-react";
+import styled from "styled-components";
 
 const ProductDetail = ({ handleClick, item }) => {
+  const StyledDiv = styled.div({
+    position: "fixed",
+    top: "50%",
+    left: "50%",
+    zIndex: "20",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: "aliceblue",
+  });
+
   return (
-    <Card sx={{ width: 500 }} className={classes.modal}>
-      <CardMedia
-        sx={{ height: 140 }}
-        image={`https://picsum.photos/200/300?random=${item.id}`}
-        title="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {item.name}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {item.meta}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {item.details}
-        </Typography>
-        <Divider variant="inset" component="li" />
-        <Typography variant="body2" color="text.secondary">
-          Rs. {item.price}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button onClick={handleClick} size="small">
-          Close
-        </Button>
-      </CardActions>
-    </Card>
+    <StyledDiv>
+      <Card sx={{ width: 500 }}>
+        <CardMedia
+          sx={{ height: 140 }}
+          image={`https://picsum.photos/200/300?random=${item.id}`}
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {item.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {item.meta}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {item.details}
+          </Typography>
+          <Divider variant="inset" component="li" />
+          <Typography variant="body2" color="text.secondary">
+            Rs. {item.price}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button onClick={handleClick} size="small">
+            Close
+          </Button>
+        </CardActions>
+      </Card>
+    </StyledDiv>
   );
 };
 
